8698e14e9faf90d9985c7fe39b31337773e3707d
sebasthub <playandroidhb@gmail.com>
2021-11-06 10:38:06 -0300

    alteracoes para delploy no heroku

--- projeto_studentwatch/projeto_studentwatch/settings.py
+++ projeto_studentwatch/projeto_studentwatch/settings.py
@@ -12,11 +12,18 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
