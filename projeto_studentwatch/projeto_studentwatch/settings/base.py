abb94561e2e91133f986bec71841ae139cdd6ab8
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2021-11-06 22:45:10 -0300

    Mais alterações

--- projeto_studentwatch/projeto_studentwatch/settings/base.py
+++ projeto_studentwatch/projeto_studentwatch/settings/base.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
