7de625e72bca3b67a893793a469c33a211fc283a
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2021-11-18 15:26:03 -0300

    Fazendo alterações para o teste de campo

--- users/forms.py
+++ users/forms.py
@@ -6,7 +6,6 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
 
 class UserForm(UserCreationForm):
-    required_css_class = 'required'
     class Meta:
         model = get_user_model()
         fields = ('username', 'email', 'password1', 'password2')
